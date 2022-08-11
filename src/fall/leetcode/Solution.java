2d2668c7a2af9513ddfa45681c8c113eedc4bad9
zhuxinyu <zhuxinyu@pcitech.com>
2022-08-11 11:07:42 +0800

    截止到 Qn 15 三数之和

--- src/fall/leetcode/Solution.java
+++ src/fall/leetcode/Solution.java
@@ -7,6 +7,9 @@ package fall.leetcode;
  */
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 //1
 //class Solution {
 //
@@ -21,7 +24,6 @@ package fall.leetcode;
 //                }
 //            }
 //        }
-//
 //        return null;
 //    }
 //
@@ -29,7 +31,26 @@ package fall.leetcode;
 //        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
 //    }
 //}
-
+//利用map更优解
+//class Solution {
+//    public static void main(String[] args) {
+//        int[] a ={3,3,3,3,1,1};
+//        for (int i : new Solution().twoSum(a, 6)) {
+//            System.out.println(i);
+//        }
+//
+//    }
+//    public int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for(int i = 0; i< nums.length; i++) {
+//            if(map.containsKey(target - nums[i])) {
+//                return new int[] {map.get(target-nums[i]),i};
+//            }
+//            map.put(nums[i], i);
+//        }
+//        throw new IllegalArgumentException("No two sum solution");
+//    }
+//}
 
 //2
 
@@ -216,38 +237,37 @@ package fall.leetcode;
 //        return ans;
 //    }
 //}
-
 //正确答案
-public class Solution {
-    public static void main(String[] args) {
-
-    }
-    private boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (!(s.charAt(start) == s.charAt(end))) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-
-    public String longestPalindrome(String s) {
-        if (s.length() < 2) {
-            return s;
-        }
-       int max = 1;
-        String maxString = s.substring(0,1);
-        for (int i = 0; i < s.length() - 1; i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (isPalindrome(s,i,j) &&  s.substring(i,j+1).length()>=max){
-                   maxString = s.substring(i,j+1);
-                   max = s.substring(i,j+1).length();
-                }
-            }
-        }
-        return maxString;
+//public class Solution {
+//    public static void main(String[] args) {
+//
+//    }
+//    private boolean isPalindrome(String s, int start, int end) {
+//        while (start < end) {
+//            if (!(s.charAt(start) == s.charAt(end))) {
+//                return false;
+//            }
+//            start++;
+//            end--;
+//        }
+//        return true;
+//    }
+//
+//    public String longestPalindrome(String s) {
+//        if (s.length() < 2) {
+//            return s;
+//        }
+//       int max = 1;
+//        String maxString = s.substring(0,1);
+//        for (int i = 0; i < s.length() - 1; i++) {
+//            for (int j = i + 1; j < s.length(); j++) {
+//                if (isPalindrome(s,i,j) &&  s.substring(i,j+1).length()>=max){
+//                   maxString = s.substring(i,j+1);
+//                   max = s.substring(i,j+1).length();
+//                }
+//            }
+//        }
+//        return maxString;
 
 
 //        int len = s.length();
@@ -282,6 +302,185 @@ public class Solution {
 //            }
 //        }
 //        return s.substring(begin, begin + maxLen);
-    }
-}
+//    }
+//}
+
+
+import java.util.*;
+
+//11. 盛最多水的容器
+
+//暴力破解 超出时间限制
+//class Solution {
+//    public static void main(String[] args) {
+//        int[] a ={2,41,4,2,3,4,2,4};
+//        System.out.println(maxArea(a));
+//    }
+//
+//    public static int maxArea(int[] height) {
+//        int x = 0;
+//        int y = height.length - 1;
+//        int maxArea = 0;
+//        while (x < y) {
+//            maxArea = Math.max((y - x) * Math.min(height[x], height[y]), maxArea);
+//            if (height[x] >= height[y]) {
+//                y--;
+//            } else {
+//                x++;
+//            }
+//        }
+//        return maxArea;
+//    }
+//}
+
+
+//42 接雨水
+
+//暴力破解1
+//class Solution {
+//    public static void main(String[] args) {
+//        Properties properties = new Properties();
+//        InputStream inputStream = Solution.class.getResourceAsStream("figure.properties");
+//        try {
+//            properties.load(inputStream);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        int[] a = {};
+//        System.out.println(new Solution().trap(a));
+//    }
+//    public int trap(int[] height) {
+//        int volume = 0;
+//        List<Integer> leftList = new ArrayList<>();
+//        List<Integer> rightList = new ArrayList<>();
+//        leftList.add(height[0]);
+//        for (int i = 2; i < height.length; i++) {
+//            rightList.add(height[i]);
+//        }
+//        for (int i = 1; i < height.length - 1; i++) {
+//            if (Math.min(Collections.max(leftList), Collections.max(rightList)) > height[i]) {
+//                volume += (Math.min(Collections.max(leftList), Collections.max(rightList)) - height[i]);
+//            }
+//            leftList.add(height[i]);
+//            rightList.remove(0);
+//        }
+//        return volume;
+//    }
+//}
+
+//暴力破解 2
+//class Solution {
+//    public static void main(String[] args) {
+//        int[] a = {1,2,2,4,2,24,1,31,2,23,2};
+//        System.out.println(new Solution().trap(a));
+//    }
+//    public int trap(int[] height) {
+//        int ans = 0;
+//        int size = height.length;
+//        for (int i = 1; i < size - 1; i++) {
+//            int max_left = 0, max_right = 0;
+//            for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+//                max_left = Math.max(max_left, height[j]);
+//            }
+//            for (int j = i; j < size; j++) { //Search the right part for max bar size
+//                max_right = Math.max(max_right, height[j]);
+//            }
+//            ans += Math.min(max_left, max_right) - height[i];
+//        }
+//        return ans;
+//    }
+//
+//}
+
+//class Solution {
+//    public static void main(String[] args) {
+//        int[] a = {4, 2, 0, 3, 2, 5};
+//        Solution solution = new Solution();
+//        int ints = solution.trap(a);
+//        System.out.println(ints);
+//    }
+
+//    public int trap(int[] height) {
+//        int volume = 0;
+//        int[] leftMax = new int[height.length];
+//        int[] rightMax = new int[height.length];
+//        leftMax[0] = height[0];
+//        rightMax[height.length - 1] = height[height.length - 1];
+//        for (int i = 1; i < height.length; i++) {
+//            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+//        }
+//        for (int i = height.length - 2; i >= 0; i--) {
+//            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+//        }
+//        for (int i = 1; i <= height.length - 2; i++) {
+//            volume += (Math.min(leftMax[i], rightMax[i]) - height[i]);
+//        }
+//        return volume;
+//    }
+//}
+////最优解
+//    public int trap(int[] height) {
+//        int volume = 0;
+//        int leftMax = 0;
+//        int rightMax = 0;
+//        int left = 0;
+//        int right = height.length - 1;
+//        while (left < right) {
+//            leftMax = Math.max(leftMax, height[left]);
+//            rightMax = Math.max(rightMax, height[right]);
+//            if (height[left] <= height[right]) {
+//                volume += leftMax - height[left];
+//                left++;
+//            } else {
+//                volume += rightMax - height[right];
+//                right--;
+//            }
+//        }
+//        return volume;
+//    }
+//            }
+
+//15 三数之和
+//最优解
+//class Solution {
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        int n = nums.length;
+//        Arrays.sort(nums);
+//        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+//        // 枚举 a
+//        for (int first = 0; first < n; ++first) {
+//            // 需要和上一次枚举的数不相同
+//            if (first > 0 && nums[first] == nums[first - 1]) {
+//                continue;
+//            }
+//            // c 对应的指针初始指向数组的最右端
+//            int third = n - 1;
+//            int target = -nums[first];
+//            // 枚举 b
+//            for (int second = first + 1; second < n; ++second) {
+//                // 需要和上一次枚举的数不相同
+//                if (second > first + 1 && nums[second] == nums[second - 1]) {
+//                    continue;
+//                }
+//                // 需要保证 b 的指针在 c 的指针的左侧
+//                while (second < third && nums[second] + nums[third] > target) {
+//                    --third;
+//                }
+//                // 如果指针重合，随着 b 后续的增加
+//                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+//                if (second == third) {
+//                    break;
+//                }
+//                if (nums[second] + nums[third] == target) {
+//                    List<Integer> list = new ArrayList<Integer>();
+//                    list.add(nums[first]);
+//                    list.add(nums[second]);
+//                    list.add(nums[third]);
+//                    ans.add(list);
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//}
 
